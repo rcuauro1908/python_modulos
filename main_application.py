c50cdf1c3064bf489a1f6458d2be1f925ecd61ea
depazjose <jose.depaz@gmail.com>
2022-10-22 13:21:52 -0500

    se agregan el proceso de tabla resta

--- main_application.py
+++ main_application.py
@@ -1,14 +1,11 @@
 #main application
 
-#imports 
+#Importar módulo a utilizar 
 import tabla_suma
-
+import tablas_resta
 
 print("imprimir tablas del 1 al 5")
 tabla_suma.generar_tablas_suma(5)
-
-#Importar módulo a utilizar
-import tablas_resta
 
 print("imprimir tablas del 1 al 5")
 tablas_resta.generar_tablas_resta(5)
